1b0ba8b0d0aadba542161c611c5ca4f6570b59bc
Nicholas Ryan Stepka <nstepka@gmail.com>
2019-06-30 10:46:07 -0500

    commit
    Sfsf

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -66,6 +66,9 @@ public class ControllerServlet extends HttpServlet {
                 case "/delete":
                     deleteBook(request, response);
                     break;
+                case "/edit":
+                    showEditForm(request, response);
+                    break;
                         
                 default:
                     listBooks(request, response);
@@ -130,6 +133,14 @@ public class ControllerServlet extends HttpServlet {
         out.println("This is the doPost() method!");
         doGet(request, response);
 
+    }
+    protected void showEditForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt((request.getParameter("id")));
+        Book existingBook = bookDAO.getBook(id);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/BookForm.jsp");
+        request.setAttribute("book", existingBook);
+        dispatcher.forward(request, response);
+        
     }
 
 }
